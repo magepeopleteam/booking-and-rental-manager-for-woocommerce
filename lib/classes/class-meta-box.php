99461d5764d6c2bf9b6fb0da12ce6c1147b14c64
Shahadat Hossain <raselsa@gmail.com>
2024-04-23 13:36:47 +0600

    refactor: metabox clean up

--- lib/classes/class-meta-box.php
+++ lib/classes/class-meta-box.php
@@ -81,124 +81,58 @@ if (!class_exists('RMFWAddMetaBox')) {
         public function meta_box_callback($post_id = null)
 
         {
-
-            $get_nav_position = $this->get_nav_position();
-            if ($this->get_meta_box_screen()[0] == 'rbfw_item' && $this->get_meta_box_context() == 'normal') { ?>
-
-                <div class="mp_tab_item" data-tab-item="#<?php echo esc_html($this->get_meta_box_id()); ?>">
-
-            <?php } ?>
-
-            <div class='wrap ppof-settings ppof-metabox'>
-                <div class='navigation <?php echo esc_attr($get_nav_position); ?>'>
-                    <div class="nav-header">
-                        <?php do_action('nav_header_top'); ?>
-                        <div class="themeName">
-                            <?php echo esc_html($this->get_item_name()); ?>
-                        </div>
-                        <div class="themeVersion">
-                            <?php echo sprintf(__('Version: %s', 'wp-theme-settings'), $this->get_item_version()); ?>
-                        </div>
-                        <?php do_action('nav_header_bottom'); ?>
-                    </div>
-
-                    <div class="nav-items">
-                        <?php
-                        do_action('nav_nav_items_top');
-                        $current_page = 1;
-                        foreach ($this->get_panels() as $page_id => $page) :
-                            $page_settings = !empty($page['sections']) ? $page['sections'] : array();
-                            $page_settings_count = count($page_settings);
-                            ?>
-
-                            <li class="nav-item-wrap <?php if (($page_settings_count > 1)) { echo 'has-child'; } ?> <?php if ($current_page == $page_id) { echo 'active';} ?>">
-                                <a dataid="<?php echo esc_attr($page_id); ?>" href='#<?php echo esc_attr($page_id); ?>' ><?php echo esc_html($page['page_nav']); ?>
-                                    <?php if (($page_settings_count > 1)) {
-                                        echo '<i class="child-nav-icon fas fa-angle-down"></i>';
-										}
-                                        ?>
-                                </a>
-                                <?php if (($page_settings_count > 1)) : ?>
-                                    <ul class="child-navs">
-                                        <?php
-                                        foreach ($page_settings as $section_id => $nav_sections) :
-                                            $nav_sections_title = !empty($nav_sections['nav_title']) ? $nav_sections['nav_title'] : $nav_sections['title'];
-                                        ?>
-                                            <li>
-                                                <a sectionId="<?php echo esc_attr($section_id); ?>" dataid="<?php echo esc_attr($page_id); ?>" href='#<?php echo esc_attr($section_id); ?>' class=' <?php if ($current_page == $page_id) { echo 'active'; } ?>'>
-                                                    <?php echo esc_html($nav_sections_title); ?>
-                                                </a>
-                                            </li>
-                                        <?php endforeach; ?>
-                                    </ul>
-                                <?php endif; ?>
-                            </li>
-
-							<?php
-                            $current_page++;
-                            endforeach;
-                            ?>
-							<?php do_action('nav_nav_items_bottom'); ?>
-						</div>
-						<div class="nav-footer">
-							<?php
-							do_action('nav_footer_top');
-							do_action('nav_footer_bottom');
-							?>
-						</div>
-					</div>
-
-					<?php
-					$current_page = 1;
-					foreach ($this->get_panels() as $panelsIndex => $panel) :
-					?>
-					<div class="tab-content <?php if ($current_page == 1) { echo 'active'; } ?>  tab-content-<?php echo esc_attr($panelsIndex); ?>">
-						<?php foreach ($panel['sections'] as $sectionIndex => $section) : ?>
-							
-							<h2 id="<?php echo esc_attr($sectionIndex); ?>" ><?php echo esc_html($section['title']); ?></h2>
-							
-							<div data-title="<?php echo esc_html($section['title']); ?>">
-								<?php foreach ($section['options'] as $option) : ?>
-									<section class="component d-flex  justify-content-between align-items-start mb-2" data-row="<?php echo $option['id']; ?>">
-										<label scope="row" class="h6 w-30">
-											<?php 
-												echo esc_html($option['title']); 
-												$details = isset($option['details']) ? $option['details'] : "";
-												if (!empty($details)) {
-													echo  '<i class="fas fa-question-circle tool-tips"><span>'.mep_esc_html($details).'</span></i>';
+			
+		?>
+            <div class="mp_tab_item" data-tab-item="#<?php echo esc_html($this->get_meta_box_id()); ?>">
+				<div class='wrap ppof-settings ppof-metabox'>
+						<?php
+						$current_page = 1;
+						foreach ($this->get_panels() as $panelsIndex => $panel) :
+						?>
+						<div class="tab-content <?php if ($current_page == 1) { echo 'active'; } ?>  tab-content-<?php echo esc_attr($panelsIndex); ?>">
+							<?php foreach ($panel['sections'] as $sectionIndex => $section) : ?>
+								
+								<h2 id="<?php echo esc_attr($sectionIndex); ?>" ><?php echo esc_html($section['title']); ?></h2>
+								
+								<div data-title="<?php echo esc_html($section['title']); ?>">
+									<?php foreach ($section['options'] as $option) : ?>
+										<section class="component d-flex  justify-content-between align-items-start mb-2" data-row="<?php echo $option['id']; ?>">
+											<label scope="row" class="h6 w-30">
+												<?php 
+													echo esc_html($option['title']); 
+													$details = isset($option['details']) ? $option['details'] : "";
+													if (!empty($details)) {
+														echo  '<i class="fas fa-question-circle tool-tips"><span>'.mep_esc_html($details).'</span></i>';
+													}
+												?>
+											</label>
+											
+											<div class="d-flex flex-column w-70">
+												<?php
+												$option_value = get_post_meta($this->get_post_id(), $option['id'], true);
+												if (is_serialized($option_value)) {
+													$option_value = unserialize($option_value);
 												}
-											?>
-										</label>
-										
-										<div class="d-flex flex-column w-70">
-											<?php
-											$option_value = get_post_meta($this->get_post_id(), $option['id'], true);
-											if (is_serialized($option_value)) {
-												$option_value = unserialize($option_value);
-											}
-											$option['value'] = $option_value;
-											$this->field_generator($option);
-											if ($option['id']=='tax_notice') {
-												printf('<p>'.mep_esc_html($details).'<p>');
-											}
-											?>
-										</div>
-									</section>
-								<?php endforeach; ?>
-							</div>
-                        <?php endforeach; ?>
-                    </div>
-                        <?php
-						$current_page++;
-						endforeach;
-                        ?>
+												$option['value'] = $option_value;
+												$this->field_generator($option);
+												if ($option['id']=='tax_notice') {
+													printf('<p>'.mep_esc_html($details).'<p>');
+												}
+												?>
+											</div>
+										</section>
+									<?php endforeach; ?>
+								</div>
+							<?php endforeach; ?>
+						</div>
+							<?php
+							$current_page++;
+							endforeach;
+							?>
+				</div>
+            
             </div>
-
-            <?php if ($this->get_meta_box_screen()[0] == 'rbfw_item' && $this->get_meta_box_context() == 'normal') { ?>
-
-            </div>
-
-        <?php } ?>
+			
             <?php
 
         }
