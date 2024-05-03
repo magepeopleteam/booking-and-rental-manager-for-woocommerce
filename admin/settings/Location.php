f909301d1779f6b819b7426273465dcb9aa7ad5c
Shahadat Hossain <raselsa@gmail.com>
2024-05-03 11:22:14 +0600

    refactor: Location class setup

--- admin/settings/Location.php
+++ admin/settings/Location.php
@@ -1,15 +1,53 @@
 <?php
 
-$rbfw_location_meta_boxs_args = array(
-			'meta_box_id'    => 'rbfw_location_config',
-			'meta_box_title' => '<i class="fa-solid fa-location-dot"></i>' .__( 'Location', 'booking-and-rental-manager-for-woocommerce' ),
-			'screen'         => array( 'rbfw_item' ),
-			'context'        => 'normal',
-			'priority'       => 'low',
-			'callback_args'  => array(),
-			'nav_position'   => 'none',
-			'item_name'      => "MagePeople",
-			'item_version'   => "2.0",
+	/*
+   * @Author 		raselsha@gmail.com
+   */
+	if (!defined('ABSPATH')) {
+		die;
+	} 
+	if (!class_exists('RBFW_Location')) {
+        class RBFW_Location{
+            public function __construct() {
+                add_action( 'rbfw_meta_box_tab_name', [$this,'add_tab_menu'] );
+                add_action( 'rbfw_meta_box_tab_content', [$this,'add_tabs_content'] );
+                add_action('save_post', array($this, 'settings_save'), 99, 1);
+			}
 
-		);
-		new RMFWAddMetaBox( $rbfw_location_meta_boxs_args );
+            public function add_tab_menu() {
+            ?>
+                <li data-target-tabs="#rbfw_location_config"><i class="fa-solid fa-location-dot"></i><?php esc_html_e('Location', 'booking-and-rental-manager-for-woocommerce' ); ?></li>
+            <?php
+            }
+
+             public function section_header(){
+                ?>
+                    <h2 class="mp_tab_item_title"><?php echo esc_html__('Tax Configuration', 'booking-and-rental-manager-for-woocommerce' ); ?></h2>
+                    <p class="mp_tab_item_description"><?php echo esc_html__('Here you can configure tax information.', 'booking-and-rental-manager-for-woocommerce' ); ?></p>
+                        
+                <?php
+            }
+
+            public function panel_header($title,$description){
+                ?>
+                    <section class="bg-light mt-5">
+                        <div>
+                            <label>
+                                <?php echo sprintf(__("%s",'booking-and-rental-manager-for-woocommerce'), $title ); ?>
+                            </label>
+                            <span><?php echo sprintf(__("%s",'booking-and-rental-manager-for-woocommerce'), $description ); ?></span>
+                        </div>
+                    </section>
+                <?php
+            }
+
+            public function add_tabs_content( $post_id ) {
+            ?>
+                <div class="mpStyle mp_tab_item" data-tab-item="#rbfw_location_config">
+					rbfw_location_config
+				</div>
+			<?php
+			}
+		}
+		new RBFW_Location();
+	}
