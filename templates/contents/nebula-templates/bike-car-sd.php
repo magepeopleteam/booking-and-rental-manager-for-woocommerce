5ae0ed1fbf182578ac8b8071a6ec43c27d62da74
Shahadat <raselsha@gmail.com>
2024-06-03 13:51:11 +0600

    feat: feature list added

--- templates/contents/nebula-templates/bike-car-sd.php
+++ templates/contents/nebula-templates/bike-car-sd.php
@@ -80,7 +80,8 @@ $review_system = rbfw_get_option('rbfw_review_system', 'rbfw_basic_review_settin
             <h2>
                 <span>$100</span>/<?php esc_html_e('day',''); ?> | <span>$10</span>/<?php esc_html_e('hr',''); ?>
             </h2>
-        </header>        
+        </header>  
+        <!-- nebula slider template -->
         <div class="rbfw-nebula-slider">
             <div class="rbfw-swiper">
                 <div class="swiper-wrapper">
@@ -107,5 +108,40 @@ $review_system = rbfw_get_option('rbfw_review_system', 'rbfw_basic_review_settin
                 </div>
             </div>
         </div>
+        <!-- title -->
+        <h2 class="title"><?php the_title(); ?></h2>
+        <div class="devider"></div>
+        <!-- Description -->
+        <div class="content"><?php the_content(); ?></div>
+        <!-- Feature icon -->
+        <div class="feature-lists">
+            <?php if ( $rbfw_feature_category ) :
+                foreach ( $rbfw_feature_category as $value ) :
+                    $cat_title = $value['cat_title'];
+                    $cat_features = $value['cat_features'] ? $value['cat_features'] : [];
+                ?>
+                <h2 class="feature-title"><?php echo esc_html($cat_title); ?></h2>
+                <div class="feature-items">
+                    <?php
+                    if(!empty($cat_features)):
+                        $i = 1;
+                        foreach ($cat_features as $features):
+                            $icon = !empty($features['icon']) ? $features['icon'] : 'fas fa-check-circle';
+                            $title = $features['title'];
+                            if($title):?>
+                                <div class="item">
+                                    <i class="<?php echo esc_attr(mep_esc_html($icon)); ?>"></i>
+                                    <h2><?php echo mep_esc_html($title); ?></h2>
+                                </div>
+                            <?php
+                            endif;
+                            $i++;
+                        endforeach;
+                    endif;
+                    ?>
+                </div>
+            <?php  endforeach; endif; ?>
+        </div>
+        <!-- Start booking -->
     </div>
 
