039f7c284497446ecc731281893daf67a3d339de
Shahadat <raselsha@gmail.com>
2024-06-03 11:43:11 +0600

    feat: slider setup

--- templates/contents/nebula-templates/bike-car-sd.php
+++ templates/contents/nebula-templates/bike-car-sd.php
@@ -49,6 +49,10 @@ $post_comfort_progress_width = function_exists('rbfw_review_get_progress_bar_wid
 $gallery_images_additional = rbfw_get_additional_gallary_images($post_id, 6);
 $prices_start_at = $rbfw->get_option('rbfw_text_prices_start_at', 'rbfw_basic_translation_settings', __('Prices start at','booking-and-rental-manager-for-woocommerce'));
 
+// -------gallery images-------
+$rbfw_gallery_images = get_post_meta($post_id,'rbfw_gallery_images');
+$gallery_images = $rbfw_gallery_images[0];
+
 /* Single Day/Appointment Type */
 $rbfw_rent_type = get_post_meta( $post_id, 'rbfw_item_type', true );
 $rbfw_bike_car_sd_data = get_post_meta( $post_id, 'rbfw_bike_car_sd_data', true );
@@ -77,75 +81,30 @@ $review_system = rbfw_get_option('rbfw_review_system', 'rbfw_basic_review_settin
                 <span>$100</span>/<?php esc_html_e('day',''); ?> | <span>$10</span>/<?php esc_html_e('hr',''); ?>
             </h2>
         </header>
+        
+               
+        
         <div class="nebula-slider">
             <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper2">
-            <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
-            </div>
-            </div>
+                <div class="swiper-wrapper">
+                    <?php 
+                        foreach($gallery_images as $key => $value):?>
+                        <div class="swiper-slide">
+                            <img src="<?php  echo wp_get_attachment_url($value ); ?>" />
+                        </div>
+                    <?php endforeach; ?>
+                </div>
             <div class="swiper-button-next"></div>
             <div class="swiper-button-prev"></div>
         </div>
         <div thumbsSlider="" class="swiper mySwiper">
             <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
-            </div>
+                <?php 
+                    foreach($gallery_images as $key => $value):?>
+                    <div class="swiper-slide">
+                        <img src="<?php  echo wp_get_attachment_url($value ); ?>" />
+                    </div>
+                <?php endforeach; ?>
             </div>
         </div>
         </div>
