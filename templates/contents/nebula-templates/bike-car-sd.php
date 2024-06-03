d64437d5cc15d1ed3eff33fd4e20040d2dcf7e61
Shahadat <raselsha@gmail.com>
2024-06-03 14:08:21 +0600

    feat; booking calender added

--- templates/contents/nebula-templates/bike-car-sd.php
+++ templates/contents/nebula-templates/bike-car-sd.php
@@ -143,5 +143,8 @@ $review_system = rbfw_get_option('rbfw_review_system', 'rbfw_basic_review_settin
             <?php  endforeach; endif; ?>
         </div>
         <!-- Start booking -->
+        <div class="start-booking">
+        <?php include( RBFW_Function::template_path( 'forms/muffin/bike-car-sd-registration.php' ) ); ?>
+        </div>
     </div>
 
