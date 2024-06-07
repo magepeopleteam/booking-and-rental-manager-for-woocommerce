07f3ba5975fc8f1084316d214ec1758539529d63
Shahadat <raselsha@gmail.com>
2024-06-07 11:05:15 +0600

    style: default color fix

--- inc/rbfw_dynamic_css.php
+++ inc/rbfw_dynamic_css.php
@@ -23,8 +23,19 @@ function rbfw_dynamic_css(){
 	$rbfw_single_page_base_color_2 = $rbfw->get_option('rbfw_single_page_base_color_2', 'rbfw_basic_style_settings');
 	$rbfw_single_page_base_color_3 = $rbfw->get_option('rbfw_single_page_base_color_3', 'rbfw_basic_style_settings');
 	$rbfw_single_page_base_color_6 = $rbfw->get_option('rbfw_single_page_base_color_6', 'rbfw_basic_style_settings');
-	$uidatepicker = rbfw_hex2rgba($rbfw_single_page_base_color_5, 0.7);
 	
+	$rent_list_base_color = $rent_list_base_color? $rent_list_base_color: '#dd3333';
+	$rbfw_single_page_base_color_4 = $rbfw_single_page_base_color_4? $rbfw_single_page_base_color_4: '#000000';
+	$rbfw_single_page_base_color_5 = $rbfw_single_page_base_color_5? $rbfw_single_page_base_color_5: '#dd3333';
+	$rbfw_single_page_secondary_color = $rbfw_single_page_secondary_color? $rbfw_single_page_secondary_color: '#333';
+	$rbfw_booking_form_bg_color = $rbfw_booking_form_bg_color? $rbfw_booking_form_bg_color: '#ddd';
+	$rbfw_single_page_base_color_1 = $rbfw_single_page_base_color_1? $rbfw_single_page_base_color_1: '#ffcd00';
+	$rbfw_single_page_base_color_2 = $rbfw_single_page_base_color_2? $rbfw_single_page_base_color_2: '#074836';
+	$rbfw_single_page_base_color_3 = $rbfw_single_page_base_color_3? $rbfw_single_page_base_color_3: '#6F1E51';
+	$rbfw_single_page_base_color_6 = $rbfw_single_page_base_color_6? $rbfw_single_page_base_color_6: '#1ABC9C';
+
+	$uidatepicker = rbfw_hex2rgba($rbfw_single_page_base_color_5, 0.7);
+
 	$inline_css =  
 	":root{
 		--rbfw_rent_list_color1:{$rent_list_base_color};  
