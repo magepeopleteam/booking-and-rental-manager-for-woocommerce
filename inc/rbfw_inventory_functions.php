cf76b6657415de50f43d58d17379621b97f0a4f0
unknown <rabiul0420@gmail.com>
2024-10-04 12:34:01 +0600

    inventory warning removed

--- inc/rbfw_inventory_functions.php
+++ inc/rbfw_inventory_functions.php
@@ -449,11 +449,12 @@ function total_service_quantity($paraent,$service,$date,$inventory,$inventory_ba
 
 
 function total_extra_service_quantity($service,$date,$inventory,$inventory_based_on_return){
-
     $total_single_service = 0;
-    foreach($inventory as $item){
-        if(in_array($date,$item['booked_dates'])  && ($item['rbfw_order_status'] == 'completed' || $item['rbfw_order_status'] == 'processing' || $item['rbfw_order_status'] == 'picked' || (($inventory_based_on_return=='yes')?$item['rbfw_order_status'] == 'returned':'') ) && isset($item['rbfw_service_info'][$service])){
-            $total_single_service += $item['rbfw_service_info'][$service];
+    if(!empty($inventory)){
+        foreach($inventory as $item){
+            if(in_array($date,$item['booked_dates'])  && ($item['rbfw_order_status'] == 'completed' || $item['rbfw_order_status'] == 'processing' || $item['rbfw_order_status'] == 'picked' || (($inventory_based_on_return=='yes')?$item['rbfw_order_status'] == 'returned':'') ) && isset($item['rbfw_service_info'][$service])){
+                $total_single_service += $item['rbfw_service_info'][$service];
+            }
         }
     }
     return $total_single_service;
