a5a7f50c88fc609c51acbc25e5f3a7283a5c7e3d
ShahnurAlam <shahnurlfe@gmail.com>
2024-11-11 09:04:00 +0600

    particular date with time

--- admin/settings/Date_Time.php
+++ admin/settings/Date_Time.php
@@ -298,11 +298,21 @@
                             <input type="text" name="rbfw_particulars[<?php echo $index; ?>][end_date]" class="rbfw_days_range" value="<?php echo esc_attr($particular['end_date'] ?? ''); ?>">
                         </td>
                         <td>
-                            <div class="w-100">
-                                <?php $this->multiple_particular_select($post_id, $index, $particular['available_time'] ?? []); ?>
-                                
-                            </div>
-                        </td>
+        <div class="w-100">
+            <?php 
+            // Render the select box with selected available time slots
+            $available_times = isset($particular['available_time']) ? $particular['available_time'] : [];
+            $rbfw_time_slots = !empty(get_option('rbfw_time_slots')) ? get_option('rbfw_time_slots') : [];
+            ?>
+            <select name="rbfw_particulars[<?php echo $index; ?>][available_time][]" multiple class="select2-hidden-accessible">
+                <?php foreach ($rbfw_time_slots as $time_slot): ?>
+                    <option value="<?php echo esc_attr($time_slot); ?>" <?php echo in_array($time_slot, $available_times) ? 'selected' : ''; ?>>
+                        <?php echo esc_html($time_slot); ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+    </td>
                         <td>
                         <button type="button" class="remove-row button" ><?php echo esc_html__('Remove', 'booking-and-rental-manager-for-woocommerce'); ?></button>
                         </td>
@@ -317,59 +327,63 @@
 
             <script>
                 jQuery(document).ready(function($) {
-                    $(".rbfw_days_range").datepicker({
-                        dateFormat: 'dd-mm-yy' 
-                    });
+    // Initialize select2 for existing select elements
+    $(".select2-hidden-accessible").select2();
 
-                                        $('#add-particular-row').click(function() {
-                        var availableTimeSlots = '<?php
-                            $rbfw_time_slots = !empty(get_option('rbfw_time_slots')) ? get_option('rbfw_time_slots') : [];
-                            $options = '';
-                            foreach ($rbfw_time_slots as $key => $value) {
-                                $options .= '<option value="' . esc_attr($value) . '">' . esc_html($key) . '</option>';
-                            }
-                            echo addslashes($options);
-                        ?>';
-                        
-                        var newRow = `
-                            <tr class="particular-row">
-                                <td><input type="text" name="rbfw_particulars[new][start_date]" class="rbfw_days_range" value=""></td>
-                                <td><input type="text" name="rbfw_particulars[new][end_date]" class="rbfw_days_range" value=""></td>
-                                <td>
-                                    <div class="w-100">
-                                        <select name="rbfw_particulars[new][available_time][]" multiple class="select2-hidden-accessible">
-                                            ${availableTimeSlots}
-                                        </select>
-                                    </div>
-                                </td>
-                                <td><button type="button" class="remove-row button">Remove</button></td>
-                            </tr>`;
+    $(".rbfw_days_range").datepicker({
+        dateFormat: 'dd-mm-yy'
+    });
 
-                        $('#particulars-table').append(newRow);
+    $('#add-particular-row').click(function() {
+        var availableTimeSlots = '<?php
+            $rbfw_time_slots = !empty(get_option('rbfw_time_slots')) ? get_option('rbfw_time_slots') : [];
+            $options = '';
+            foreach ($rbfw_time_slots as $key => $value) {
+                $options .= '<option value="' . esc_attr($value) . '">' . esc_html($key) . '</option>';
+            }
+            echo addslashes($options);
+        ?>';
+        
+        var newRow = `
+            <tr class="particular-row">
+                <td><input type="text" name="rbfw_particulars[new][start_date]" class="rbfw_days_range" value=""></td>
+                <td><input type="text" name="rbfw_particulars[new][end_date]" class="rbfw_days_range" value=""></td>
+                <td>
+                    <div class="w-100">
+                        <select name="rbfw_particulars[new][available_time][]" multiple class="select2-hidden-accessible">
+                            ${availableTimeSlots}
+                        </select>
+                    </div>
+                </td>
+                <td><button type="button" class="remove-row button">Remove</button></td>
+            </tr>`;
 
-                        $(".rbfw_days_range").datepicker({
-                            dateFormat: 'dd-mm-yy'
-                        });
-                        $('#particulars-table').find('tr:last select').select2();
-                        
-                    });
+        $('#particulars-table').append(newRow);
 
-                    $(document).on('click', '.remove-row', function() {
-                        $(this).closest('.particular-row').remove();
-                    });
+        $(".rbfw_days_range").datepicker({
+            dateFormat: 'dd-mm-yy'
+        });
 
-                    $('input[name=rbfw_particular_switch]').click(function() {
-                        var status = $(this).val();
-                        if (status == 'on') {
-                            $(this).val('off');
-                            $('.available-particular').slideUp().removeClass('show').addClass('hide');
-                        } 
-                        if(status == 'off') {
-                            $(this).val('on'); 
-                            $('.available-particular').slideDown().removeClass('hide').addClass('show');
-                        }
-                    });
-                });
+        $('#particulars-table').find('tr:last select').select2();
+    });
+
+    $(document).on('click', '.remove-row', function() {
+        $(this).closest('.particular-row').remove();
+    });
+
+    $('input[name=rbfw_particular_switch]').click(function() {
+        var status = $(this).val();
+        if (status == 'on') {
+            $(this).val('off');
+            $('.available-particular').slideUp().removeClass('show').addClass('hide');
+        } 
+        if(status == 'off') {
+            $(this).val('on'); 
+            $('.available-particular').slideDown().removeClass('hide').addClass('show');
+        }
+    });
+});
+
             </script>
             <?php
 }
