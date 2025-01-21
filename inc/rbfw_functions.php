e4c12b8f66b8490744fec3d04060d82bf473f1a0
unknown <rabiul0420@gmail.com>
2025-01-21 10:47:02 +0600

    fixed

--- inc/rbfw_functions.php
+++ inc/rbfw_functions.php
@@ -2941,6 +2941,22 @@
 		}
 	}
 
+function rbfw_array_strip( $array_or_string ) {
+    if ( is_string( $array_or_string ) ) {
+        $array_or_string = sanitize_text_field( $array_or_string );
+    } elseif ( is_array( $array_or_string ) ) {
+        foreach ( $array_or_string as $key => &$value ) {
+            if ( is_array( $value ) ) {
+                $value = rbfw_array_strip( $value );
+            } else {
+                $value = sanitize_text_field( $value );
+            }
+        }
+    }
+
+    return $array_or_string;
+}
+
 function sanitize_post_array($data, $rules) {
     $sanitized_data = [];
 
