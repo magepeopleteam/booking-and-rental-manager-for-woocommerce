ccb72dad4b67c8f7d1893d7e85c126dcd8191bb1
unknown <rabiul0420@gmail.com>
2025-03-11 10:16:58 +0600

    seassional pricing solved

--- inc/class-bike-car-md-function.php
+++ inc/class-bike-car-md-function.php
@@ -71,6 +71,8 @@ if ( ! class_exists( 'RBFW_BikeCarMd_Function' ) ) {
             $max_available_qty = rbfw_get_multiple_date_available_qty($post_id, $start_date, $end_date,'',$pickup_datetime,$dropoff_datetime,$rbfw_enable_time_slot);
             $duration_price_info = rbfw_md_duration_price_calculation($post_id,$pickup_datetime,$dropoff_datetime,$start_date,$end_date,$star_time,$end_time,$rbfw_enable_time_slot);
 
+           // echo '<pre>';print_r($duration_price_info);echo '<pre>';exit;
+
             $duration_price = $duration_price_info['duration_price'] * $item_quantity;
             $total_days = $duration_price_info['total_days'];
             $actual_days = $duration_price_info['actual_days'];
