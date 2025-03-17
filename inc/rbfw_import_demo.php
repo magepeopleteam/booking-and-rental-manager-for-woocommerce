7cf89cff4ee4b0034fdb3d3d1420424dc63ea30b
Shahadat <raselsha@gmail.com>
2025-03-17 11:54:24 +0600

    quick set up fix

--- inc/rbfw_import_demo.php
+++ inc/rbfw_import_demo.php
@@ -10,71 +10,71 @@
 		class RbfwImportDemo {
 			public function __construct() {
 				
-				// $sample_rent_items = get_option( 'rbfw_sample_rent_items' );
-				// if ( $sample_rent_items != 'imported' ) {
-				// 	$this->rbfw_import_demo_function();
-				// }
+				$sample_rent_items = get_option( 'rbfw_sample_rent_items' );
+				if ( $sample_rent_items != 'imported' ) {
+					$this->rbfw_import_demo_function();
+				}
 
 			}
 
-public function rbfw_import_demo_function() {
-    // If you must ensure longer execution time, consider handling this at the server level.
-    
-    $xml_url     = RBFW_PLUGIN_URL . '/assets/sample-rent-items.xml';
-    $xml         = simplexml_load_file( $xml_url );
-    $json_string = wp_json_encode( $xml );
-    $xml_array   = json_decode( $json_string, true );
-    $xml_array = ! empty( $xml_array['item'] ) ? $xml_array['item'] : [];
-    
-    if ( $xml !== false && ! empty( $xml_array ) ) {
-        $counter = count( $xml_array );
-        $i = 1;
-        foreach ( $xml_array as $item ) {
-            if ( $i <= $counter ) {
-                $title   = ! empty( $item['title'] ) ? $item['title'] : '';
-                $content = ! empty( $item['content'] ) ? $item['content'] : '';
-                unset($rent_args);
-                $rent_args = array(
-                    'post_title'   => $title,
-                    'post_content' => $content,
-                    'post_status'  => 'publish',
-                    'post_type'    => 'rbfw_item',
-                );
-                $rent_post_id = wp_insert_post( $rent_args );
-                $rent_post_metas = ! empty( $item['postmeta'] ) ? $item['postmeta'] : '';
-                
-                if ( ! empty( $rent_post_id ) ) {
-                    foreach ( $rent_post_metas as $value ) {
-                        $meta_key = $value['meta_key'];
-                        $meta_value = ! empty( $value['meta_value'] ) ? maybe_unserialize( $value['meta_value'] ) : '';
-                        update_post_meta( $rent_post_id, $meta_key, $meta_value );
-                    }
-                    
-                    $rbfw_bkp_gallary_imgs = get_post_meta( $rent_post_id, 'rbfw_bkp_gallary_imgs', true );
-                    $rbfw_bkp_thumb_img    = get_post_meta( $rent_post_id, 'rbfw_bkp_thumb_img', true );
-                    $gallary_arr = [];
-                    
-                    foreach ( $rbfw_bkp_gallary_imgs as $url ) {
-                        $attach_id     = $this->rbfw_media_upload_from_url( $url );
-                        $gallary_arr[] = $attach_id;
-                    }
-                    update_post_meta( $rent_post_id, 'rbfw_gallery_images', $gallary_arr );
-                    update_post_meta( $rent_post_id, 'rbfw_gallery_images_additional', $gallary_arr );
-                    
-                    $thumb_id = '';
-                    foreach ( $rbfw_bkp_thumb_img as $url ) {
-                        $attach_id = $this->rbfw_media_upload_from_url( $url );
-                        $thumb_id  = $attach_id;
-                    }
-                    update_post_meta( $rent_post_id, '_thumbnail_id', $thumb_id );
-                }
-            }
-            $i++;
-        }
-        $this->rbfw_update_related_products();
-        update_option( 'rbfw_sample_rent_items', 'imported' );
-    }
-}
+			public function rbfw_import_demo_function() {
+				// If you must ensure longer execution time, consider handling this at the server level.
+				
+				$xml_url     = RBFW_PLUGIN_URL . '/assets/sample-rent-items.xml';
+				$xml         = simplexml_load_file( $xml_url );
+				$json_string = wp_json_encode( $xml );
+				$xml_array   = json_decode( $json_string, true );
+				$xml_array = ! empty( $xml_array['item'] ) ? $xml_array['item'] : [];
+
+				if ( $xml !== false && ! empty( $xml_array ) ) {
+					$counter = count( $xml_array );
+					$i = 1;
+					foreach ( $xml_array as $item ) {
+						if ( $i <= $counter ) {
+							$title   = ! empty( $item['title'] ) ? $item['title'] : '';
+							$content = ! empty( $item['content'] ) ? $item['content'] : '';
+							unset($rent_args);
+							$rent_args = array(
+								'post_title'   => $title,
+								'post_content' => $content,
+								'post_status'  => 'publish',
+								'post_type'    => 'rbfw_item',
+							);
+							$rent_post_id = wp_insert_post( $rent_args );
+							$rent_post_metas = ! empty( $item['postmeta'] ) ? $item['postmeta'] : '';
+							
+							if ( ! empty( $rent_post_id ) ) {
+								foreach ( $rent_post_metas as $value ) {
+									$meta_key = $value['meta_key'];
+									$meta_value = ! empty( $value['meta_value'] ) ? maybe_unserialize( $value['meta_value'] ) : '';
+									update_post_meta( $rent_post_id, $meta_key, $meta_value );
+								}
+								
+								$rbfw_bkp_gallary_imgs = get_post_meta( $rent_post_id, 'rbfw_bkp_gallary_imgs', true );
+								$rbfw_bkp_thumb_img    = get_post_meta( $rent_post_id, 'rbfw_bkp_thumb_img', true );
+								$gallary_arr = [];
+								
+								foreach ( $rbfw_bkp_gallary_imgs as $url ) {
+									$attach_id     = $this->rbfw_media_upload_from_url( $url );
+									$gallary_arr[] = $attach_id;
+								}
+								update_post_meta( $rent_post_id, 'rbfw_gallery_images', $gallary_arr );
+								update_post_meta( $rent_post_id, 'rbfw_gallery_images_additional', $gallary_arr );
+								
+								$thumb_id = '';
+								foreach ( $rbfw_bkp_thumb_img as $url ) {
+									$attach_id = $this->rbfw_media_upload_from_url( $url );
+									$thumb_id  = $attach_id;
+								}
+								update_post_meta( $rent_post_id, '_thumbnail_id', $thumb_id );
+							}
+						}
+						$i++;
+					}
+					$this->rbfw_update_related_products();
+					update_option( 'rbfw_sample_rent_items', 'imported' );
+				}
+			}
 
 
 			public function rbfw_update_related_products() {
