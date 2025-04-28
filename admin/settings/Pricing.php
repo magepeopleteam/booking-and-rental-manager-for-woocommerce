82b2d776c4224729b5b76309bd290c8472e085ca
unknown <rabiul0420@gmail.com>
2025-04-28 09:59:25 +0600

    resort sessional

--- admin/settings/Pricing.php
+++ admin/settings/Pricing.php
@@ -675,12 +675,12 @@
                         </table>
                     </section>
                     <br>
-					<?php do_action( 'rbfw_after_rent_item_type_table_row' ); ?>
 
 					<?php do_action( 'rbfw_after_general_price_table_row' ); ?>
 
 					<?php do_action( 'rbfw_after_general_price_table', $post_id ); ?>
                 </div>
+                <?php do_action( 'rbfw_after_rent_item_type_table_row' ); ?>
 				<?php do_action( 'rbfw_after_week_price_table', $post_id ); ?>
 				<?php do_action( 'rbfw_after_extra_service_table' ); ?>
 				<?php
