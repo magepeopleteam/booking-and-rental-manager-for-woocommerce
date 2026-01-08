1dbb31a63ddc0f8808f26172c6167a21fe3c5563
Shahadat <raselsha@gmail.com>
2026-01-08 09:39:09 +0600

    style: update pricing info

--- assets/mp_script/md_script.js
+++ assets/mp_script/md_script.js
@@ -55,11 +55,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 jQuery(document).on('mouseenter','.rbfw-pricing-info-heading i',function() {
-    jQuery('.price-item-container').stop(true, true).slideDown();
+    jQuery('.price-item-container').stop(true, true).addClass('open').fadeIn();
+    jQuery(this).closest('.mp_right_section').addClass('overlay');
+    jQuery(this).closest('.rbfw-pricing-info-heading').addClass('open');
 });
 
 jQuery(document).on('mouseleave', '.rbfw-pricing-info-heading i', function () {
-    jQuery('.price-item-container').stop(true, true).slideUp();
+    jQuery('.price-item-container').stop(true, true).removeClass('open').fadeOut();
+    jQuery(this).closest('.mp_right_section').removeClass('overlay');
+    jQuery(this).closest('.rbfw-pricing-info-heading').removeClass('open');
+});
+
+ // Click → toggle
+jQuery(document).on('click', '.rbfw-pricing-info-heading i', function (e) {
+    e.preventDefault();
+    jQuery(this).closest('.rbfw-pricing-info-heading').toggleClass('open');
+    jQuery(this).closest('.mp_right_section').toggleClass('overlay');
+    jQuery('.price-item-container').stop(true, true).toggleClass('open').fadeToggle();
 });
 
 jQuery('body').on('focusin', '.pickup_date', function(e) {
