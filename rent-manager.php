ba45834dce7264b311a8d95c8a6b11f2b509a93f
magepeopleteam <magepeoplemagepeople@gmail.com>
2026-06-15 10:12:58 +0600

    chore: remove outdated repo header from plugin metadata

--- rent-manager.php
+++ rent-manager.php
@@ -10,7 +10,6 @@
 	 * License: GPL v2 or later
 	 * License URI: https://www.gnu.org/licenses/gpl-2.0.html
 	 * Domain Path: /languages/
-	 * Repo: rental
 	 */
 	if ( ! defined( 'ABSPATH' ) ) {
 		die;
